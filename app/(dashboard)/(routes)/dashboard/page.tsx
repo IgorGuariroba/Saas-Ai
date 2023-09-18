84c4a6ec7f666d570cd9b0b1c0fbdcf45d28a1ec
igor.guariroba <igor.guariroba@jsl.com.br>
2023-09-18 15:56:05 -0300

    Add dynamic sidebar and dashboard layout
    Added dynamic sidebar and dashboard layout to the application. Created individual card components for each tool that will be used in creating AI-managed assets. In addition, unused SVG files were removed, and new UI components like navbar, sheet and mobile sidebar were introduced for better user interface and easy navigation. These changes ensure a seamless transition between routes inside the dashboard and better requesting and retrieving of data from the back-end.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,84 @@
+"use client"
+
+import {
+    ArrowRight,
+    VideoIcon,
+    ImageIcon,
+    Music,
+    Code2Icon,
+    MessageSquare
+} from "lucide-react";
+import {Card} from "@/components/ui/card";
+import {cn} from "@/lib/utils";
+
+const tools = [
+    {
+        label: "Conversar",
+        icon: MessageSquare,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/conversation"
+    },
+    {
+        label: "Gerar Imagem",
+        icon: ImageIcon,
+        color: "text-pink-700",
+        bgColor: "bg-pink-700/10",
+        href: "/image"
+    },
+    {
+        label: "Criar Video",
+        icon: VideoIcon,
+        color: "text-orange-500",
+        bgColor: "bg-orange-500/10",
+        href: "/video"
+    },
+    {
+        label: "Criar Musica",
+        icon: Music,
+        color: "text-emerald-500",
+        bgColor: "bg-emerald-500/10",
+        href: "/image"
+    },
+    {
+        label: "Gerar Código",
+        icon: Code2Icon,
+        color: "text-violet-500",
+        bgColor: "bg-violet-500/10",
+        href: "/image"
+    },
+]
 const DashboardPage = () => {
     return (
-        <>
-            <p>Dashboard Page (protected)</p>
-        </>
+        <div>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold text-center">
+                    Explore o poder da Mente Digital
+                </h2>
+                <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+                    converse com IA - Experimente o seu poder
+                </p>
+            </div>
+            <div className="px-4 md:px-20 lg:px-32 space-y-4">
+                {tools.map((tool) =>  (
+                    <Card key={tool.href+tool.label}
+                          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+                    >
+                        <div className=" flex items-center gap-x-4">
+                            <div className={cn("p-2 w-fit rounded-md",
+                                tool.bgColor)}>
+                                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+                            </div>
+                            <div className="font-semibold">
+                                {tool.label}
+                            </div>
+                        </div>
+
+                        <ArrowRight className="w-5 h-5"/>
+                    </Card>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <>
             Landing Page (Unprotected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Logar
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Cadastrar-se
+                    </Button>
+                </Link>
+            </div>
         </>
     );
 };
